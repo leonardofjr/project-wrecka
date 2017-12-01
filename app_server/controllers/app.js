c67a9790efc0f7eca2b521710c6604ebc8550616
Leonardo Felipa <leonardo.f.jr@gmail.com>
2017-12-01 18:36:17 -0500

    Resetted Default Stylesheet & Added Bootstrap Styles

--- app_server/controllers/app.js
+++ app_server/controllers/app.js
@@ -1,3 +1,8 @@
 module.exports.index = function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { 
+        title: 'Express',
+        metaAuthor: 'Leonardo Felipa',
+        metaDescription: 'Boilerplate',
+        metaKeywords: 'Boilerplate, HTML5, CSS3, ExpressJS, NodeJS, Mongo, Mongoose, Angular'
+    });
 }
