f32285b05b03d23a919080e7f66feac89e671f1c
leonardofjr <leonardo.f.jr@gmail.com>
2018-01-12 17:53:08 -0500

    Added Logo, Sample Of Main Image, Added Scrolling Animation Using WoW & Animate-CSS

--- app_server/controllers/app.js
+++ app_server/controllers/app.js
@@ -1,6 +1,6 @@
 module.exports.index = function(req, res, next) {
     res.render('index', { 
-        title: 'Express',
+        title: 'Triple C Automotive',
         metaAuthor: 'Leonardo Felipa',
         metaDescription: 'Boilerplate',
         metaKeywords: 'Boilerplate, HTML5, CSS3, ExpressJS, NodeJS, Mongo, Mongoose, Angular'
